24a699e3c51ee921919eb56c9512989c2d956b80
JManuelLorenzo <juanma.lorenzo@correo.ucu.edu.uy>
2024-11-15 17:02:34 -0300

    Hice que los ataques infligan los estados de manera correspondientes

--- src/Library/StaticClasses/AttackGenerator.cs
+++ src/Library/StaticClasses/AttackGenerator.cs
@@ -55,23 +55,84 @@ namespace Library.StaticClasses
         private static List<IAttack> GetAttacksByType(string type)
         {
             var attacksByType = new Dictionary<string, List<IAttack>>()
-            {
-                { "Plant", new List<IAttack> { new Attack("Vine Whip", 10, 35, "Plant"), new Attack("Solar Beam", 35, 60, "Plant"), new Attack("Leaf Storm", 60, 100, "Plant") }},
-                { "Water", new List<IAttack> { new Attack("Water Gun", 10, 35, "Water"), new Attack("Hydro Pump", 35, 60, "Water"), new Attack("Aqua Tail", 60, 100, "Water") }},
-                { "Bug", new List<IAttack> { new Attack("Bug Bite", 10, 35, "Bug"), new Attack("Signal Beam", 35, 60, "Bug"), new Attack("X-Scissor", 60, 100, "Bug") }},
-                { "Dragon", new List<IAttack> { new Attack("Dragon Claw", 10, 35, "Dragon"), new Attack("Dragon Tail", 35, 60, "Dragon"), new Attack("Draco Meteor", 60, 100, "Dragon") }},
-                { "Electric", new List<IAttack> { new Attack("Thunder Shock", 10, 35, "Electric"), new Attack("Spark", 35, 60, "Electric"), new Attack("Thunderbolt", 60, 100, "Electric") }},
-                { "Ghost", new List<IAttack> { new Attack("Shadow Ball", 10, 35, "Ghost"), new Attack("Night Shade", 35, 60, "Ghost"), new Attack("Hex", 60, 100, "Ghost") }},
-                { "Fire", new List<IAttack> { new Attack("Ember", 10, 35, "Fire"), new Attack("Flamethrower", 35, 60, "Fire"), new Attack("Fire Blast", 60, 100, "Fire") }},
-                { "Ice", new List<IAttack> { new Attack("Ice Shard", 10, 35, "Ice"), new Attack("Blizzard", 35, 60, "Ice"), new Attack("Frost Breath", 60, 100, "Ice") }},
-                { "Fight", new List<IAttack> { new Attack("Karate Chop", 10, 35, "Fight"), new Attack("Dynamic Punch", 35, 60, "Fight"), new Attack("Close Combat", 60, 100, "Fight") }},
-                { "Normal", new List<IAttack> { new Attack("Tackle", 10, 35, "Normal"), new Attack("Body Slam", 35, 60, "Normal"), new Attack("Hyper Beam", 60, 100, "Normal") }},
-                { "Psychic", new List<IAttack> { new Attack("Psybeam", 10, 35, "Psychic"), new Attack("Psychic", 35, 60, "Psychic"), new Attack("Future Sight", 60, 100, "Psychic") }},
-                { "Rock", new List<IAttack> { new Attack("Rock Throw", 10, 35, "Rock"), new Attack("Rock Slide", 35, 60, "Rock"), new Attack("Stone Edge", 60, 100, "Rock") }},
-                { "Earth", new List<IAttack> { new Attack("Mud-Slap", 10, 35, "Earth"), new Attack("Earthquake", 35, 60, "Earth"), new Attack("Bulldoze", 60, 100, "Earth") }},
-                { "Venom", new List<IAttack> { new Attack("Poison Sting", 10, 35, "Venom"), new Attack("Sludge Bomb", 35, 60, "Venom"), new Attack("Venom Shock", 60, 100, "Venom") }},
-                { "Flying", new List<IAttack> { new Attack("Wing Attack", 10, 35, "Flying"), new Attack("Aerial Ace", 35, 60, "Flying"), new Attack("Sky Attack", 60, 100, "Flying") }}
-            };
+{
+    { "Plant", new List<IAttack> { 
+        new Attack("Vine Whip", 10, 0, "Plant"), 
+        new Attack("Solar Beam", 35, 0, "Plant"), 
+        new Attack("Leaf Storm", 60, 0, "Plant") 
+    }},
+    { "Water", new List<IAttack> { 
+        new Attack("Water Gun", 10, 0, "Water"), 
+        new Attack("Hydro Pump", 35, 0, "Water"), 
+        new Attack("Aqua Tail", 60, 0, "Water") 
+    }},
+    { "Bug", new List<IAttack> { 
+        new Attack("Bug Bite", 10, 0, "Bug"), 
+        new Attack("Signal Beam", 35, 3, "Bug"),  // Paraliza
+        new Attack("X-Scissor", 60, 0, "Bug") 
+    }},
+    { "Dragon", new List<IAttack> { 
+        new Attack("Dragon Claw", 10, 0, "Dragon"), 
+        new Attack("Dragon Tail", 35, 0, "Dragon"), 
+        new Attack("Draco Meteor", 60, 0, "Dragon") 
+    }},
+    { "Electric", new List<IAttack> { 
+        new Attack("Thunder Shock", 10, 3, "Electric"),  // Paraliza
+        new Attack("Spark", 35, 3, "Electric"),  // Paraliza
+        new Attack("Thunderbolt", 60, 3, "Electric")  // Paraliza
+    }},
+    { "Ghost", new List<IAttack> { 
+        new Attack("Shadow Ball", 10, 0, "Ghost"), 
+        new Attack("Night Shade", 35, 0, "Ghost"), 
+        new Attack("Hex", 60, 0, "Ghost") 
+    }},
+    { "Fire", new List<IAttack> { 
+        new Attack("Ember", 10, 1, "Fire"),  // Quema
+        new Attack("Flamethrower", 35, 1, "Fire"),  // Quema
+        new Attack("Fire Blast", 60, 1, "Fire")  // Quema
+    }},
+    { "Ice", new List<IAttack> { 
+        new Attack("Ice Shard", 10, 0, "Ice"), 
+        new Attack("Blizzard", 35, 0, "Ice"), 
+        new Attack("Frost Breath", 60, 0, "Ice") 
+    }},
+    { "Fight", new List<IAttack> { 
+        new Attack("Karate Chop", 10, 0, "Fight"), 
+        new Attack("Dynamic Punch", 35, 5, "Fight"),  // Confunde
+        new Attack("Close Combat", 60, 0, "Fight") 
+    }},
+    { "Normal", new List<IAttack> { 
+        new Attack("Tackle", 10, 0, "Normal"), 
+        new Attack("Body Slam", 35, 4, "Normal"),  // Duerme
+        new Attack("Hyper Beam", 60, 0, "Normal") 
+    }},
+    { "Psychic", new List<IAttack> { 
+        new Attack("Psybeam", 10, 0, "Psychic"), 
+        new Attack("Psychic", 35, 0, "Psychic"), 
+        new Attack("Future Sight", 60, 0, "Psychic") 
+    }},
+    { "Rock", new List<IAttack> { 
+        new Attack("Rock Throw", 10, 0, "Rock"), 
+        new Attack("Rock Slide", 35, 0, "Rock"), 
+        new Attack("Stone Edge", 60, 0, "Rock") 
+    }},
+    { "Earth", new List<IAttack> { 
+        new Attack("Mud-Slap", 10, 0, "Earth"), 
+        new Attack("Earthquake", 35, 0, "Earth"), 
+        new Attack("Bulldoze", 60, 0, "Earth") 
+    }},
+    { "Venom", new List<IAttack> { 
+        new Attack("Poison Sting", 10, 2, "Venom"),  // Envenena
+        new Attack("Sludge Bomb", 35, 2, "Venom"),  // Envenena
+        new Attack("Venom Shock", 60, 2, "Venom")  // Envenena
+    }},
+    { "Flying", new List<IAttack> { 
+        new Attack("Wing Attack", 10, 0, "Flying"), 
+        new Attack("Aerial Ace", 35, 0, "Flying"), 
+        new Attack("Sky Attack", 60, 0, "Flying") 
+    }}
+};
+
 
             if (!attacksByType.ContainsKey(type))
                 throw new ArgumentException("Tipo de Pokémon no reconocido.");
